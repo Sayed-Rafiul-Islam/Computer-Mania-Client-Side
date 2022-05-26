17b75497b1162c27853f5eb13ad8ea434da684b5
Yasin-Rafid <96468660+Yasin-Rafid@users.noreply.github.com>
2022-05-26 23:45:35 +0600

    done

--- src/Pages/ContactUS/ContactUs.js
+++ src/Pages/ContactUS/ContactUs.js
@@ -3,18 +3,18 @@ import React from 'react';
 const ContactUs = () => {
     return (
         <div className='my-8'>
-            <h1 class="text-5xl font-bold text-primary">Contact Us</h1>
-            <div class="hero">
+            <h1 className="text-5xl font-bold text-primary">Contact Us</h1>
+            <div className="hero">
 
-                <div class="hero-content flex-col lg:flex-row">
+                <div className="hero-content flex-col lg:flex-row">
                     <img className='w-96' src="https://i.ibb.co/mvNZ4Gy/contact-us.png" />
                     <div className='text-left'>
 
-                        <p class="mt-4">Phone : <span className='text-primary'>+8801645797223 or 01955569777</span></p>
-                        <p class="mt-2 mb-3">Email : <span className='text-primary'>computermenia@yahoo.com</span></p>
-                        <label class="mt-5">Write us : </label> <br />
-                        <textarea class="textarea textarea-primary w-full mt-2" placeholder="How can we serve you?"></textarea> <br />
-                        <button class="btn btn-primary w-full mt-2">Send Text</button>
+                        <p className="mt-4">Phone : <span className='text-primary'>+8801645797223 or 01955569777</span></p>
+                        <p className="mt-2 mb-3">Email : <span className='text-primary'>computermenia@yahoo.com</span></p>
+                        <label className="mt-5">Write us : </label> <br />
+                        <textarea className="textarea textarea-primary w-full mt-2" placeholder="How can we serve you?"></textarea> <br />
+                        <button className="btn btn-primary w-full mt-2">Send Text</button>
                     </div>
                 </div>
             </div>
--- src/Pages/Summery/Summery.js
+++ src/Pages/Summery/Summery.js
@@ -6,24 +6,24 @@ const Summery = () => {
             <h1 className='text-primary text-3xl my-4'>
                 <b> Summery</b>
             </h1>
-            <div class="stats stats-vertical lg:stats-horizontal shadow">
+            <div className="stats stats-vertical lg:stats-horizontal shadow">
 
-                <div class="stat">
-                    <div class="stat-title">We Served</div>
-                    <div class="stat-value">1000+</div>
-                    <div class="stat-desc">CUstomers</div>
+                <div className="stat">
+                    <div className="stat-title">We Served</div>
+                    <div className="stat-value">1000+</div>
+                    <div className="stat-desc">CUstomers</div>
                 </div>
 
-                <div class="stat">
-                    <div class="stat-title">New Users</div>
-                    <div class="stat-value">4,200</div>
-                    <div class="stat-desc">↗︎ 400 (22%)</div>
+                <div className="stat">
+                    <div className="stat-title">New Users</div>
+                    <div className="stat-value">4,200</div>
+                    <div className="stat-desc">↗︎ 400 (22%)</div>
                 </div>
 
-                <div class="stat">
-                    <div class="stat-title">We provide</div>
-                    <div class="stat-value">100+</div>
-                    <div class="stat-desc">Parts and Products</div>
+                <div className="stat">
+                    <div className="stat-title">We provide</div>
+                    <div className="stat-value">100+</div>
+                    <div className="stat-desc">Parts and Products</div>
                 </div>
 
             </div>
